7db31415a4809c9cb6ab8314e5b0b5afed2ef6b1
Omar Ali AbdelSalam <excalibur1987@gmail.com>
2024-12-15 07:56:10 +0300

    Add Express.js server setup and update dependencies

---
+++ server.js
@@ -0,0 +1,29 @@
+import { rmSync, existsSync } from "fs";
+import { handler } from "./build/handler.js";
+
+import path from "path";
+
+import express from "express";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(handler);
+
+const serverBinding = process.env?.["SERVER_BINDING"];
+
+if (isNaN(parseInt(serverBinding))) {
+  if (serverBinding !== path.basename(serverBinding)) {
+    throw new Error("Server binding is missing or invalid!");
+  }
+  if (existsSync(serverBinding.toString())) {
+    rmSync(serverBinding.toString());
+  }
+}
+
+app.listen(serverBinding, () => {
+  console.log(`listening on ${serverBinding}`);
+});
